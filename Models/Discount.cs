4db60ca6dfe70aa7f686d5e96c627839cf9c62e6
kiryl <zhydalovich.kiryl@gmail.com>
2024-06-26 18:44:17 +0200

    Made new database scaffold

---
+++ Models/Discount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProjectAPBD.Models;
+
+public partial class Discount
+{
+    public int DiscountId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal DiscountPercentage { get; set; }
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public virtual ICollection<Contract> Contracts { get; set; } = new List<Contract>();
+}
