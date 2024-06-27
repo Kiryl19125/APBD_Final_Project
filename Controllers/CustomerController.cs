9874fb61e86d014bb4766a72f0ff883e93a314f2
kiryl <zhydalovich.kiryl@gmail.com>
2024-06-27 10:42:49 +0200

    Made and tested customer controller

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 using FinalProjectAPBD.Context;
+using FinalProjectAPBD.Models;
 using FinalProjectAPBD.Models.RequestModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -17,7 +18,7 @@ public class CustomerController : ControllerBase
         _context = context;
     }
 
-    // [Authorize(Roles = "admin")]
+    [Authorize(Roles = "admin")]
     [HttpDelete("deleteCustomer")]
     public async Task<IActionResult> DeleteCustomer(DeleteCustomerModel model)
     {
@@ -32,10 +33,49 @@ public class CustomerController : ControllerBase
         return NotFound($"customer with PESEL: {model.PESEL} not fount");
     }
 
-    // [Authorize(Roles = "admin")]
-    // [HttpPost("updateCustomerData")]
-    // public IActionResult UpdateCustomerData()
-    // {
-    //     
-    // }
+    [Authorize(Roles = "admin")]
+    [HttpPut("updateCustomerData")]
+    public async Task<IActionResult> UpdateCustomerData(UpdateCustomerModel model)
+    {
+        var customer = await _context.Customers.SingleOrDefaultAsync(c => c.Pesel == model.PESEL);
+        if (customer != null)
+        {
+            if (customer.IsDeleted)
+            {
+                return BadRequest($"customer with PESEL: {model.PESEL} already deleted");
+            }
+
+            customer.FirstName = model.FirstName;
+            customer.LastName = model.LastName;
+            customer.Address = model.Address;
+            customer.Email = model.Email;
+            customer.PhoneNumber = model.PhoneNumber;
+            await _context.SaveChangesAsync();
+
+            return Ok($"customer with PESEL: {model.PESEL} updated successfully");
+        }
+
+        return NotFound($"customer with PESEL: {model.PESEL} not fount");
+    }
+
+    [HttpPost("addNewCustomer")]
+    public async Task<IActionResult> AddNewCustomer(AddNewCustomerModel model)
+    {
+        var newCustomer = new Customer()
+        {
+            Pesel = model.Pesel,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            Address = model.Address,
+            Email = model.Email,
+            PhoneNumber = model.PhoneNumber,
+            IsDeleted = false
+        };
+
+        await _context.Customers.AddAsync(newCustomer);
+        await _context.SaveChangesAsync();
+
+        return Ok("new customer was added successfully.");
+
+    }
 }
---
+++ Models/RequestModels/UpdateCustomerModel.cs
@@ -0,0 +1,12 @@
+namespace FinalProjectAPBD.Models.RequestModels;
+
+public class UpdateCustomerModel
+{
+    public string PESEL { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    
+}
